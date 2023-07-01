d8c66f1056b5be3d3badcef99527c76399a31d7e
Sylvan Martin <alephnulltim@gmail.com>
2023-07-01 14:38:19 -0400

    Created cryptographic primitives

---
+++ Sources/LatticeKit/LWE/LWE.swift
@@ -0,0 +1,8 @@
+//
+//  LWE.swift
+//  
+//
+//  Created by Sylvan Martin on 7/1/23.
+//
+
+import Foundation
---
+++ Sources/LatticeKit/Primitives.swift
@@ -0,0 +1,55 @@
+//
+//  Primitives.swift
+//
+//  A collection of protocols describing cryptographic primitives
+//
+//  Created by Sylvan Martin on 7/1/23.
+//
+
+import Foundation
+
+/**
+ * A public key encryption scheme
+ */
+protocol PKEScheme {
+    
+    // MARK: Types
+    
+    /**
+     * The secret key type of this scheme
+     */
+    associatedtype SecretKey
+    
+    /**
+     * The public key type of this scheme
+     */
+    associatedtype PublicKey
+    
+    /**
+     * The plaintext type of this scheme
+     */
+    associatedtype Plaintext
+    
+    /**
+     * The ciphertext type of this scheme
+     */
+    associatedtype Ciphertext
+    
+    // MARK: Methods
+    
+    /**
+     * Generates a public and secret key pair
+     */
+    func gen() -> (SecretKey, PublicKey)
+    
+    /**
+     * Encrypts a `Plaintext` given a `PublicKey`
+     */
+    func enc(_: PublicKey, plaintext: Plaintext) -> Ciphertext
+    
+    /**
+     * Decrypts a `Ciphertext` from a given `SecretKey`
+     */
+    func dec(_: SecretKey, ciphertext: Ciphertext) -> Plaintext
+    
+}
